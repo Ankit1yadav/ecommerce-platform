1ca5029c5ea2dec24b528c0c644c54488f58eae4
ankyadav13 <yadav.ankit71@gmail.com>
2025-07-08 13:10:58 +0530

    Added feature changes

--- product-service/src/main/java/com/ecom/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/ecom/productservice/repository/ProductRepository.java
@@ -1,8 +1,27 @@
 package com.ecom.productservice.repository;
 
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
-import com.ecom.productservice.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.HashMap;
+import java.util.Map;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+@Repository
+public class ProductRepository {
+    @Autowired
+    private DynamoDbClient dynamoDbClient;
+
+    private static final String TABLE_NAME = "ecommerce-products";
+
+    public void saveProduct(Map<String, AttributeValue> item) {
+        Map<String, AttributeValue> request = new HashMap<>(item);
+        PutItemRequest putItemRequest = PutItemRequest.builder()
+                .tableName(TABLE_NAME)
+                .item(request)
+                .build();
+        dynamoDbClient.putItem(putItemRequest);
+    }
 }
